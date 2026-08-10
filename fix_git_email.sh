027c2bd01b84ba1793919071e61621e86f3114e6
Akim <rlawoals623@gmail.com>
2026-08-10 21:55:49 +0900

    fix_git_email.sh

---
+++ fix_git_email.sh
@@ -0,0 +1,19 @@
+OLD_EMAIL="$1@gmail.com"
+CORRECT_NAME="AkimJemi"
+CORRECT_EMAIL="$2@gmail.com"
+
+git filter-branch --env-filter "
+if [ \"$GIT_COMMITTER_EMAIL\" = '$OLD_EMAIL' ]
+then
+    export GIT_COMMITTER_NAME='$CORRECT_NAME'
+    export GIT_COMMITTER_EMAIL='$CORRECT_EMAIL'
+fi
+if [ \"$GIT_AUTHOR_EMAIL\" = '$OLD_EMAIL' ]
+then
+    export GIT_AUTHOR_NAME='$CORRECT_NAME'
+    export GIT_AUTHOR_EMAIL='$CORRECT_EMAIL'
+fi
+" --tag-name-filter cat -- --branches --tags
+
+git log --oneline --author-date-order
+git push origin main --force
